51a63395286b449a113b4054a87a305519d5e6d4
Milan Markovic <milan.markovic1b@gmail.com>
2026-02-09 12:06:32 +0100

    added now a String buttonText array and check for button

--- Calculator/src/main/MainPanel.java
+++ Calculator/src/main/MainPanel.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.Button;
 import java.awt.GridLayout;
 
 import java.awt.event.ActionEvent;
@@ -15,6 +16,12 @@ public class MainPanel extends JPanel implements ActionListener {
 
 	CalculationPanel calculatorPanel;
 
+	
+	// each button used for the calculator
+	String[] buttonText = {"AC","DEL","+","-","0","1",
+			"2","*","3","4","5","÷","6","7","8","(-/+)",
+			".","9","!"};
+	
 	/**
 	 * 
 	 * @param calculatorPanel used to display the current calculation and solution
@@ -26,15 +33,15 @@ public class MainPanel extends JPanel implements ActionListener {
 		this.calculatorPanel = calculatorPanel;
 
 		int layoutRows = 0;
-		int layoutColumns = 3;
-		int layoutHGap = 10;
-		int layoutVGap = 10;
+		int layoutColumns = 4;
+		int layoutHGap = 3;
+		int layoutVGap = 3;
 
 		setLayout(new GridLayout(layoutRows, layoutColumns, layoutHGap, layoutVGap));
 
 		buttonList = new ArrayList<JButton>();
 
-		createButtons(9);
+		createButtons();
 
 	}
 
@@ -43,26 +50,60 @@ public class MainPanel extends JPanel implements ActionListener {
 	 * 
 	 * @param numberOfButtons how many Buttons should be created
 	 */
-	public void createButtons(int numberOfButtons) {
+	public void createButtons() {
 
-		for (int i = 0; i <= numberOfButtons; i++) {
+		for (int i = 0; i < buttonText.length; i++) {
 
 			JButton btn = new JButton();
 
 			btn.addActionListener(this);
 
-			btn.setText("" + i);
+			String btnText = buttonText[i];
+			
+			btn.setText(btnText);
+			
+			btn.setFocusable(false);
 
 			add(btn);
 
 		}
 
 	}
-
+	
+	public void setCalculationText(String Text) {
+		
+		String bufferText = calculatorPanel.getCalculation();
+		
+		Text = bufferText + Text;
+		
+		System.out.println(Text);
+		
+		calculatorPanel.setCalculation(Text);
+		
+	}
+	
+	
+	public void checkButton(String buttonValue) {
+		
+		switch (buttonValue){
+			case "0","1","2","3","4","5","6","7","8","9",".": 
+				setCalculationText(buttonValue);
+				break;
+			case "+":
+				
+			default:// default is the all clear button
+				
+				break;
+		}
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		System.out.println("clicked");
-
+		JButton source = (JButton) e.getSource();
+		
+		String buttonValue = source.getText();
+		
+		checkButton(buttonValue);
 	}
 }
